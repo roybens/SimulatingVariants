f672493754b5c81d6109b4ec936ae0db77341514
Jinan Jiang <jinan789@outlook.com>
2021-03-19 03:24:44 -0700

    Added more value calculations to the scoring function

--- scoring_functions_relative.py
+++ scoring_functions_relative.py
@@ -23,14 +23,6 @@ class Score_Function:
         self.ramp_protocol = protocols_dict['ramp']
         self.persistent_protocol = protocols_dict['persistent']
 
-    def get_values_from_gensim():
-        gen_data = eh.gen_sim_data()
-
-        self.gv_slope = linregress(gen_data['act sweeps'], gen_data['act'])[0]
-        self.tau_0 = gen_data['tau0']
-        
-
-
 
     def dv_half_act(self, plus_minus_wild):
 
@@ -55,9 +47,35 @@ class Score_Function:
     def persistent(self, percent_wild):
 
 
-}
+    def get_values_from_gensim():
+        gen_data = eh.gen_sim_data()
 
+        def find_half_and_slope(v_vec, ipeak_vec):
+            """ Returns V50 and slope
+                Notes:
+                    gpeak_max = gpeak_vec.max() maximum value of the conductance used to normalize the conductance vector
+            """
+            # convert to numpy arrays
+            v_vec = np.array(v_vec)
+            ipeak_vec = np.array(ipeak_vec)
 
+            # find start of linear portion (0 mV and onwards)
+            inds = np.where(v_vec >= 0)
 
+            # take linear portion of voltage and current relationship
+            lin_v = v_vec[inds]
+            lin_i = ipeak_vec[inds]
+            
+            #boltzmann for conductance
+            def boltzmann(vm, Gmax, v_half, s):
+                vrev = stats.linregress(lin_i, lin_v).intercept
+                return Gmax * (vm - vrev) / (1 + np.exp((v_half - vm) / s))
 
+            Gmax, v_half, s = optimize.curve_fit(boltzmann, v_vec, ipeak_vec)[0]
+            return v_half, s
 
+        self.gv_slope, self.dv_half_act = find_half_and_slope(gen_data['act sweeps'], gen_data['act'])
+        self.ssi_slope, self.dv_half_ssi = find_half_and_slope(gen_data['inact sweeps'], gen_data['inact'])
+        self.tau_0 = gen_data['tau0']
+        #self.udb20 ignore it for now
+        #ramp and persistent not ready yet
