30d9fedf73524f75a22838607683aa7af3988a74
Michael Lam <mikelam.us@berkeley.edu>
2021-03-25 19:18:24 -0700

    Changed some names

--- curve_fitting.py
+++ curve_fitting.py
@@ -9,13 +9,13 @@ import eval_helper as eh
 import generalized_genSim_shorten_time as ggsd
 from scipy import optimize, stats
 
-def fit_sigmoid(x, slope, v_half, top, bottom):
+def boltzmann(x, slope, v_half, top, bottom):
     '''
     Fit a sigmoid curve to the array of datapoints.
     '''
     return bottom +  ((top - bottom) / (1.0 + np.exp((v_half - x)/slope)))
 
-def fit_two_phase_association(x, y0, plateau, percent_fast, k_fast, k_slow):
+def two_phase(x, y0, plateau, percent_fast, k_fast, k_slow):
     '''
     Fit a two-phase association curve to an array of data points X. 
     For info about the parameters, visit 
@@ -44,7 +44,7 @@ class Curve_Fitter:
         v_array = np.array(v_vec)
         ssi_slope, v_half, top, bottom = self.calc_inact_obj()
         even_xs = np.linspace(v_array[0], v_array[len(v_array)-1], 100)
-        curve = fit_sigmoid(even_xs, ssi_slope, v_half, top, bottom)
+        curve = boltzmann(even_xs, ssi_slope, v_half, top, bottom)
         axs[0].set_xlabel('Voltage (mV)')
         axs[0].set_ylabel('Fraction Inactivated')
         axs[0].set_title("Inactivation Curve")
@@ -60,7 +60,7 @@ class Curve_Fitter:
         v_array = np.array(v_vec)
         gv_slope, v_half, top, bottom = self.calc_act_obj()
         even_xs = np.linspace(v_array[0], v_array[len(v_array)-1], 100)
-        curve = fit_sigmoid(even_xs, gv_slope, v_half, top, bottom)
+        curve = boltzmann(even_xs, gv_slope, v_half, top, bottom)
         axs[1].set_xlabel('Voltage (mV)')
         axs[1].set_ylabel('Fraction Activated')
         axs[1].set_title("Activation Curve")
@@ -70,20 +70,20 @@ class Curve_Fitter:
         axs[1].text(-10, 0.3, 'V50: ' + str(v_half) + ' mV')
         axs[1].legend()
         
-        # Recovery Curve
-        #axs[2].set_xlabel('Log(Time)')
-        #axs[2].set_ylabel('Fractional Recovery')
-        #axs[2].set_title("Recovery from Inactivation")
-        #even_xs = np.linspace(times[0], times[len(times)-1], 100)
-        #curve = fit_two_phase_association(even_xs, *popt)
-        #axs[2].plot(np.log(even_xs), curve, c=fit_color,label="Recovery Fit")
-        #axs[2].scatter(np.log(times), data_pts, label='Recovery', color=fit_color,marker=curr_marker)
+        Recovery Curve
+        axs[2].set_xlabel('Log(Time)')
+        axs[2].set_ylabel('Fractional Recovery')
+        axs[2].set_title("Recovery from Inactivation")
+        even_xs = np.linspace(times[0], times[len(times)-1], 100)
+        curve = two_phase(even_xs, *popt)
+        axs[2].plot(np.log(even_xs), curve, c=fit_color,label="Recovery Fit")
+        axs[2].scatter(np.log(times), data_pts, label='Recovery', color=fit_color,marker=curr_marker)
         plt.show()
 
     def calc_act_obj(self):
         gnorm_vec, v_vec, all_is = self.activation.genActivation()
         try:
-            popt, pcov = optimize.curve_fit(fit_sigmoid, v_vec, gnorm_vec)
+            popt, pcov = optimize.curve_fit(boltzmann, v_vec, gnorm_vec)
         except:
             print("Very bad voltages in activation.")
             return
@@ -94,7 +94,7 @@ class Curve_Fitter:
     def calc_inact_obj(self):
         inorm_vec, v_vec, all_is = self.inactivation.genInactivation()
         try:
-            popt, pcov = optimize.curve_fit(fit_sigmoid, v_vec, inorm_vec)
+            popt, pcov = optimize.curve_fit(boltzmann, v_vec, inorm_vec)
         except:
             print("Very bad voltages in inactivation.")
             return
