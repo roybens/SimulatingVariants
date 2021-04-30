abaeab30f56468d8dc2789304c51bc7ff65aab0f
Jinan Jiang <jinan789@outlook.com>
2021-04-30 13:22:58 -0700

    Now the notebook supports saving all figures as pdf under the same directory

--- exec_plot_choice.py
+++ exec_plot_choice.py
@@ -1,7 +1,9 @@
 from generalized_genSim_shorten_time import *
 from eval_helper import *
+from fpdf import FPDF
+import os
 
-def exec_plot_choice(plot_choice, new_params = False):
+def exec_plot_choice(plot_choice, new_params = False, save_plots_as_pdf = False):
     """Renders the plot of choice
     
     args:
@@ -10,9 +12,11 @@ def exec_plot_choice(plot_choice, new_params = False):
         None
     """
 
+    #updates parameters if given any
     if new_params:
         change_params_dict(new_params)
 
+    #plots each function
     if plot_choice == "Activation":
         genAct = Activation()
         genAct.genActivation()
@@ -36,5 +40,17 @@ def exec_plot_choice(plot_choice, new_params = False):
         genRFIdv.genRecInactTauCurve_dv()
         genRFIdv.plotRecInact_dv()
         genRFIdv.plotRecInactProcedure_dv()
+
+    #saves the plots as pdf in the same directory
+    if save_plots_as_pdf:
+        plot_path = os.path.abspath(os.getcwd()) + '/Plots_Folder'
+        all_plots_name = os.listdir(plot_path)
+
+        pdf = FPDF()
+        pdf.add_page()
+        for image in [plot_path + '/' + f for f in all_plots_name if f.startswith(plot_choice)]:
+            pdf.image(image)
+
+        pdf.output(plot_choice + "_plot.pdf", "F")
 
     
